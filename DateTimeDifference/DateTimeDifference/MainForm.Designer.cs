ead4c731df41d3abd6f11bbf818aedbf56932ddc
aquatyan <44610889+aquatyan@users.noreply.github.com>
2019-04-16 09:35:15 +0300

    Add files via upload

---
+++ DateTimeDifference/DateTimeDifference/MainForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace DateTimeDifference
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbResult = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(187, 210);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(187, 267);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(208, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите первую дату";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(208, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите вторую дату";
+            // 
+            // txbResult
+            // 
+            this.txbResult.Location = new System.Drawing.Point(255, 309);
+            this.txbResult.Name = "txbResult";
+            this.txbResult.Size = new System.Drawing.Size(83, 20);
+            this.txbResult.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(357, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "дн.";
+            // 
+            // btnResult
+            // 
+            this.btnResult.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnResult.Location = new System.Drawing.Point(164, 307);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(85, 23);
+            this.btnResult.TabIndex = 7;
+            this.btnResult.Text = "Результат";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(244, 362);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Выйти";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(534, 511);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txbResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Программа";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ DateTimeDifference/DateTimeDifference/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DateTimeDifference
+{
+    public partial class MainForm : Form
+    {
+        Point moveStart; // точка для перемещения
+        public MainForm()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            string DateC;
+            txbResult.Text = DateC = Convert.ToString(((dateTimePicker2.Value - dateTimePicker1.Value).Days));
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath =
+new System.Drawing.Drawing2D.GraphicsPath();
+            // создаем эллипс с высотой и шириной формы
+            myPath.AddEllipse(0, 0, this.Width, this.Height);
+            // создаем с помощью элипса ту область формы, которую мы хотим видеть
+            Region myRegion = new Region(myPath);
+            // устанавливаем видимую область
+            this.Region = myRegion;
+        }
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                moveStart = new Point(e.X, e.Y);
+            }
+        }
+
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            // если нажата левая кнопка мыши
+            if ((e.Button & MouseButtons.Left) != 0)
+            {
+                // получаем новую точку положения формы
+                Point deltaPos = new Point(e.X - moveStart.X, e.Y - moveStart.Y);
+                // устанавливаем положение формы
+                this.Location = new Point(this.Location.X + deltaPos.X,
+                this.Location.Y + deltaPos.Y);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(this, "Exit?", "Закрытие программы", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.No)
+            {
+                this.Close();
+            }
+        }
+    }
+}
